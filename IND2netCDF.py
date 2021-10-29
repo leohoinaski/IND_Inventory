Fixing units and netcdf creation

IND2netCDF.py
@@ -6,7 +6,8 @@
                              
                              
 This is the main script to convert industrial emission from .csv file 
-to netCDF.
+to chemical speciated  and temporal disaggregated netCDF files.
+
 
 Inputs: 
     
@@ -29,28 +30,36 @@ Inputs:
     year: Base-year for your simulation
             
     fileId = identification of your output files
+    
+    
+Input file:
+    
+    BR_Ind.xlsx - excel file with industrial characteristics, emissions, 
+        and coordinates. Emissions are in kg/s.
        
 
 Outputs:
     
     'INDannualEmiss_'+fileId+'_'+str(deltaX)+'x'+str(deltaY)+'_'+str(year)+'.nc'
-    
+        (unit in g/year)
     
 External functions:
     gridding, populatingGrid, netCDFcreator
     
-Last update = 21/10/2021
+    
+    
+Last update = 29/10/2021
 
 Author: Leonardo Hoinaski - leonardo.hoinaski@ufsc.br
 
----------------------------------------------------------------
+-------------------------------------------------------------------------------
 """
 
 import geopandas as gpd
 import pandas as pd
 import os 
 import numpy as np
-from netCDFcreator import createNETCDF, createNETCDFtemporal
+from netCDFcreator import createNETCDFtemporal
 #from temporalDisagregation import temporalDisagVehicular
 import numpy.matlib
 from shapely.geometry import Polygon
@@ -76,9 +85,9 @@ loni = -54 #loni = int(round(bound.minx)) # Initial longitude (lower-left)
 
 lonf = -47 #lonf = int(round(bound.maxx)) # Final longitude (upper-right)
 
-deltaX = 0.01 # Grid resolution/spacing in x direction
+deltaX = 0.05 # Grid resolution/spacing in x direction
 
-deltaY = 0.01 # Grig resolution/spacing in y direction
+deltaY = 0.05 # Grig resolution/spacing in y direction
 
 fileId = 'SC' # Code to identify your output files
 
@@ -87,6 +96,9 @@ prefix = str(deltaX)+'x'+str(deltaY) # grid definition identification
 year = 2019
 
 month = 1
+
+convKg2g = 1000 # Conversion from kg/s to g/s
+
 
 #%%------------------------- PROCESSING ---------------------------------------
 
@@ -116,12 +128,18 @@ dataEmissIND = ind[['PMemis',
                     'SOxemis',
                     'VOCemis']].copy() 
 
+# Converting from kg/s to g/s
+dataEmissIND=dataEmissIND*convKg2g
+
 # Estimated data
 dataEmissINDestmeas = ind[['PMestmeas', 
                     'COestmeas', 
                     'NOxestmeas',
                     'SOxestmeas',
-                    'VOCestmeas']].copy() 
+                    'VOCestmeas']].copy()
+
+# Converting from kg/s to g/s
+dataEmissINDestmeas=dataEmissINDestmeas*convKg2g 
 
 # Substituting nan by estimated data
 isna = dataEmissIND.isna()
@@ -187,7 +205,13 @@ dates['year'] = year
 dates['month'] = 1
 dates['day'] = 1
 dates['hour'] = 00
-createNETCDFtemporal(outPath,name,dataIND,xv,yv,y,x,dates,month)
+# Getting number of days in a year
+fullDate = np.arange(np.datetime64(datetime.datetime(year, 1, 1, 0, 0)),
+                     np.datetime64(datetime.datetime(year, 12, 31, 0, 0)),
+                     3600000000*24)
+# Conversion from second to year
+convSec2year = fullDate.shape[0]*24*60*60 
+createNETCDFtemporal(outPath,name,dataIND*convSec2year,xv,yv,y,x,dates,month,'Annual')
 
 #%% Calling netcdf creator
 